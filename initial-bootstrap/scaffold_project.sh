609d572ef0b2586e1fdbe6588984439568445bf1
Tim Wang <tianxun.wang1@nhs.net>
2022-09-28 10:47:14 +0100

    added scaffold script for initial boostrap

---
+++ initial-bootstrap/scaffold_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yo aws-api:initial-bootstrap-terraform nrlf \
+    eu-west-2 \
+    nrlf-terraform-state \
+    nrlf-terraform-state-lock \
+    483433329243 mgmt NHSDAdminRole \
+    295962215401 terraform NHSDAdminRole\
+    295962215401 terraform NHSDAdminRole\
+    295962215401 terraform NHSDAdminRole
