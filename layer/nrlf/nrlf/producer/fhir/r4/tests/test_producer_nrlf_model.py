c16d53843fe3ab8818658845b5b05b1872d96a37
Joel Klinger <joel.klinger1@nhs.net>
2022-10-10 13:47:37 +0100

    [DALA-1220] Pin test data, add R4 in addition to R4b

--- layer/nrlf/nrlf/producer/fhir/r4/tests/test_producer_nrlf_model.py
+++ layer/nrlf/nrlf/producer/fhir/r4/tests/test_producer_nrlf_model.py
@@ -7,31 +7,27 @@ import pydantic
 import pytest
 from nrlf.producer.fhir.r4.model import DocumentReference
 
-PATH_TO_HERE = Path(__file__).parent
+PATH_TO_TEST_DATA = Path(__file__).parent / "data"
+TEST_DATA_RELATIVE_PATHS = ["nrlf", "www.hl7.org/fhir/R4", "www.hl7.org/fhir/R4B"]
 
 
 @cache
-def _good_nrlf_document_reference():
-    with open(PATH_TO_HERE / "producer_nrlf_fhir.json") as f:
+def read_test_data(relative_path: str):
+    with open(
+        PATH_TO_TEST_DATA / relative_path / "documentreference-example.json"
+    ) as f:
         return json.load(f)
 
 
-@pytest.fixture()
-def good_nrlf_document_reference():
-    document_reference = _good_nrlf_document_reference()
-    return deepcopy(document_reference)
+@pytest.mark.parametrize("relative_path", TEST_DATA_RELATIVE_PATHS)
+def test_good_nrlf_document_is_valid(relative_path):
+    document_reference = read_test_data(relative_path=relative_path)
+    DocumentReference(**document_reference)
 
 
-@pytest.fixture()
-def bad_nrlf_document_reference(good_nrlf_document_reference):
-    good_nrlf_document_reference["type"] = "an invalid value!"
-    return good_nrlf_document_reference
-
-
-def test_good_nrlf_document_is_valid(good_nrlf_document_reference):
-    DocumentReference(**good_nrlf_document_reference)
-
-
-def test_bad_nrlf_document_is_not_valid(bad_nrlf_document_reference):
+@pytest.mark.parametrize("relative_path", TEST_DATA_RELATIVE_PATHS)
+def test_bad_nrlf_document_is_valid(relative_path):
+    document_reference = deepcopy(read_test_data(relative_path=relative_path))
+    document_reference["type"] = "an invalid value!"
     with pytest.raises(pydantic.ValidationError):
-        DocumentReference(**bad_nrlf_document_reference)
+        DocumentReference(**document_reference)
