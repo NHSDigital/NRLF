3cd405d0ed9bc979f91a5600892d4dde10d5a5ff
james.linnell1 <james.linnell1@hscic.gov.uk>
2022-10-19 15:12:01 +0100

    linting fix

--- api/consumer/readDocumentReference/src/versioning.py
+++ api/consumer/readDocumentReference/src/versioning.py
@@ -1,7 +1,11 @@
-from layer.lambda_utils.lambda_utils.header_config import AcceptHeader as BaseAcceptHeader
+from layer.lambda_utils.lambda_utils.header_config import (
+    AcceptHeader as BaseAcceptHeader,
+)
+
 
 class AcceptHeader(BaseAcceptHeader):
     pass
+
 
 def get_version_from_header(event) -> str:
     accept_header = AcceptHeader(event)
