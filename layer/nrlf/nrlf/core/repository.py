6e637d5ec5be53abe09d8f8d762f3e3b46eb609c
Brian Bayes <brian.bayes@live.co.uk>
2022-10-21 10:59:27 +0100

    NRLF-108 Create, Read, Update Methods (#17)
    Create, Read and Update methods implemented on repository.py
    
    Co-authored-by: Brian Bayes <brianbayes@Brians-MacBook-Pro.local>
    Co-authored-by: Rowan Gill <rowan_gill@hotmail.co.uk>

---
+++ layer/nrlf/nrlf/core/repository.py
@@ -0,0 +1,56 @@
+import re
+from nrlf.core.dynamodb_types import DynamoDbType
+from nrlf.core.validators import make_timestamp
+import boto3
+from pydantic import BaseModel
+
+
+def _to_snake_case(name: str) -> str:
+    return re.sub(r"(?<!^)(?=[A-Z])", "-", name).lower()
+
+
+class Repository:
+    def __init__(self, item_type: BaseModel, client):
+        self.dynamodb = client
+        self.item_type = item_type
+        self.table_name = _to_snake_case(item_type.__name__)
+        for key, value in item_type.__fields__.items():
+            if not issubclass(value.type_, DynamoDbType):
+                print(key, value.type_)
+                raise TypeError(
+                    "Model contains fields that are not of type DynamoDbType"
+                )
+
+    def create(self, item: BaseModel):
+        return self.dynamodb.put_item(TableName=self.table_name, Item=item)
+
+    def read(self, **kwargs):
+        response = self.dynamodb.get_item(TableName=self.table_name, Key=kwargs)
+        return self.item_type.construct(**response["Item"])
+
+    def search(self, key: any):
+        return NotImplementedError
+
+    def update(self, item: BaseModel):
+        return self.dynamodb.put_item(TableName=self.table_name, Item=item)
+
+    def supersede(self, item: BaseModel):
+        return NotImplementedError
+
+    def soft_delete_document_pointer(self, id: str, status=None):
+        update_expression = "SET deleted_on = :now"
+        expression_attribute = {":now": {"S": make_timestamp()}}
+
+        if status is not None:
+            updateExpression += ", status = :status"
+            expression_attribute[":status"] = {"S": status}
+
+        return self.dynamodb.update_item(
+            Key={"id": id},
+            TableName="document-pointer",
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_attribute,
+        )
+
+    def hard_delete(self, id: str):
+        return self.dynamodb.delete_item(TableName=self.table_name, Key={"id": id})
---
+++ layer/nrlf/nrlf/core/tests/test_repository.py
@@ -0,0 +1,187 @@
+from contextlib import contextmanager
+from http import client
+from unittest.mock import mock_open
+from pydantic import BaseModel
+from nrlf.core.validators import make_timestamp
+import moto
+import json
+from nrlf.core.model import DocumentPointer, create_document_pointer_from_fhir_json
+from nrlf.producer.fhir.r4.tests.test_producer_nrlf_model import read_test_data
+from nrlf.core.repository import Repository
+import boto3
+import pytest
+from datetime import datetime
+
+DEFAULT_ATTRIBUTE_DEFINITIONS = [{"AttributeName": "id", "AttributeType": "S"}]
+DEFAULT_KEY_SCHEMA = [{"AttributeName": "id", "KeyType": "HASH"}]
+
+
+@contextmanager
+def mock_dynamodb(table_name):
+    with moto.mock_dynamodb():
+        client = boto3.client("dynamodb")
+        client.create_table(
+            AttributeDefinitions=DEFAULT_ATTRIBUTE_DEFINITIONS,
+            TableName=table_name,
+            KeySchema=DEFAULT_KEY_SCHEMA,
+            BillingMode="PAY_PER_REQUEST",
+        )
+        yield client
+
+
+def test_fields_are_not_all_dynamo_db_type():
+    # Arrange
+    class TestClass(BaseModel):
+        field: str
+
+    table_name = "document-pointer"
+
+    with pytest.raises(TypeError) as error, mock_dynamodb(table_name) as client:
+        repository = Repository(TestClass, client)
+
+    # Assert
+    (message,) = error.value.args
+    assert message == "Model contains fields that are not of type DynamoDbType"
+
+
+def test_fields_are_all_dynamo_db_type():
+    # Arrange
+    table_name = "document-pointer"
+
+    with mock_dynamodb(table_name):
+        Repository(DocumentPointer, client)
+
+
+def test_create_document_pointer():
+    # Arrange
+    document_reference = json.dumps(read_test_data("nrlf"))
+    api_version = 1
+    core_model = create_document_pointer_from_fhir_json(
+        raw_fhir_json=document_reference, api_version=api_version
+    )
+    table_name = "document-pointer"
+
+    with mock_dynamodb(table_name) as client:
+        repository = Repository(DocumentPointer, client)
+
+        # Act
+        repository.create(item=core_model.dict())
+        response = client.scan(TableName=table_name)
+
+    (item,) = response["Items"]
+    recovered_item = DocumentPointer.construct(**item)
+
+    # Assert
+    assert recovered_item.dict() == core_model.dict()
+
+
+def test_read_document_pointer():
+    # Arrange
+    document_reference = json.dumps(read_test_data("nrlf"))
+    api_version = 1
+    core_model = create_document_pointer_from_fhir_json(
+        raw_fhir_json=document_reference, api_version=api_version
+    )
+    table_name = "document-pointer"
+
+    with mock_dynamodb(table_name) as client:
+        repository = Repository(DocumentPointer, client)
+        repository.create(item=core_model.dict())
+
+        # Act
+        result = repository.read(id=core_model.id.value)
+
+    # Assert
+    assert core_model == result
+
+
+def test_update_document_pointer():
+    # Arrange
+    document_reference = json.dumps(read_test_data("nrlf"))
+
+    updated_document = read_test_data("nrlf")
+    updated_document["content"][0]["attachment"][
+        "url"
+    ] = "https://example.org/different_doc.pdf"
+
+    updated_document_reference = json.dumps(updated_document)
+
+    api_version = 1
+
+    core_model = create_document_pointer_from_fhir_json(
+        raw_fhir_json=document_reference, api_version=api_version
+    )
+    updated_core_model = create_document_pointer_from_fhir_json(
+        raw_fhir_json=updated_document_reference, api_version=api_version
+    )
+    table_name = "document-pointer"
+
+    with mock_dynamodb(table_name) as client:
+        repository = Repository(DocumentPointer, client)
+        repository.create(item=core_model.dict())
+
+        # Act
+        repository.update(item=updated_core_model.dict())
+        response = client.scan(TableName=table_name)
+
+    (item,) = response["Items"]
+    recovered_item = DocumentPointer.construct(**item)
+
+    # Assert
+    assert recovered_item.dict() == updated_core_model.dict()
+
+
+def test_soft_delete():
+    # Arrange
+    document_reference = json.dumps(read_test_data("nrlf"))
+
+    api_version = 1
+    core_model = create_document_pointer_from_fhir_json(
+        raw_fhir_json=document_reference, api_version=api_version
+    )
+    table_name = "document-pointer"
+
+    with mock_dynamodb(table_name) as client:
+        repository = Repository(DocumentPointer, client)
+        repository.create(item=core_model.dict())
+
+        # Act
+        repository.soft_delete_document_pointer(core_model.id.value)
+        response = client.scan(TableName=table_name)
+
+    (item,) = response["Items"]
+
+    recovered_item = DocumentPointer.construct(**item)
+    now = datetime.now().date()
+    deleted_date = datetime.strptime(
+        recovered_item.deleted_on["S"], "%Y-%m-%dT%H:%M:%S.%fZ"
+    ).date()
+
+    # Assert
+    assert now == deleted_date
+
+
+def test_hard_delete():
+    # Arrange
+    document_reference = json.dumps(read_test_data("nrlf"))
+
+    api_version = 1
+    core_model = create_document_pointer_from_fhir_json(
+        raw_fhir_json=document_reference, api_version=api_version
+    )
+    table_name = "document-pointer"
+
+    with mock_dynamodb(table_name) as client:
+        repository = Repository(DocumentPointer, client)
+        repository.create(item=core_model.dict())
+
+        # Act
+        repository.hard_delete(core_model.id.value)
+        response = client.scan(TableName=table_name)
+
+    # Assert
+    assert len(response["Items"]) == 0
+
+
+def test_search():
+    pass
