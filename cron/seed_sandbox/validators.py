29916e9368ec4bd52c67dacecdbc19050a651309
Brian Bayes <brian.bayes@live.co.uk>
2023-01-26 12:04:23 +0000

    [NRLF-264] Refactored validation to resolve issues raised by testers

---
+++ cron/seed_sandbox/validators.py
@@ -0,0 +1,30 @@
+import json
+
+from nrlf.core.model import DocumentPointer
+from nrlf.producer.fhir.r4.model import DocumentReference
+from pydantic import BaseModel, ValidationError
+
+
+def validate_items(items: list[BaseModel]):
+    valid_items = list()
+
+    for item in items:
+        try:
+            if _is_item_valid(item):
+                valid_items.append(item)
+        except ValidationError:
+            continue
+
+    return valid_items
+
+
+def _is_item_valid(item: BaseModel):
+    try:
+        if type(item) == DocumentPointer:
+            DocumentReference(**json.loads(item.document.__root__))
+            return True
+        else:
+            return True
+    except ValidationError as e:
+        print(f"Validation Error for item {item}. Error is {e}")
+        return False
