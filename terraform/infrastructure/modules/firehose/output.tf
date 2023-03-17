67886ae2432ee24a3a660e8a1cfd517c0a81f2a7
Joel Klinger <joel.klinger1@nhs.net>
2023-03-17 10:05:03 +0000

    [NRLF-355] add firehose/cloudwatch subscription to api/auth/status lambdas

--- terraform/infrastructure/modules/firehose/output.tf
+++ terraform/infrastructure/modules/firehose/output.tf
@@ -8,3 +8,17 @@ output "delivery_stream" {
     }
   }
 }
+
+output "firehose_subscription" {
+  value = {
+    destination = {
+      arn = aws_kinesis_firehose_delivery_stream.firehose.arn
+    }
+    role = {
+      arn = aws_iam_role.firehose_subscription.arn
+    }
+    filter = {
+      pattern = "[lambda_log_type != \"INIT_START\" && lambda_log_type != \"START\" && lambda_log_type != \"END\" && lambda_log_type != \"REPORT\", everything_else]"
+    }
+  }
+}
