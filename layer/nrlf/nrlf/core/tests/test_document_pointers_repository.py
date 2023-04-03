68f59974108e00b6e4a5977c300ff8932bfd00a3
Joel Klinger <joel.klinger1@nhs.net>
2023-04-03 16:02:19 +0100

    [NRLF-376] calc last evaluated key once

--- layer/nrlf/nrlf/core/tests/test_document_pointers_repository.py
+++ layer/nrlf/nrlf/core/tests/test_document_pointers_repository.py
@@ -1,6 +1,8 @@
 import json
 import time
 from contextlib import contextmanager
+from itertools import chain
+from math import ceil
 from typing import Generator
 
 import boto3
@@ -17,6 +19,7 @@ from nrlf.core.errors import (
 from nrlf.core.model import DocumentPointer, key
 from nrlf.core.repository import (
     MAX_TRANSACT_ITEMS,
+    PAGE_ITEM_LIMIT,
     Repository,
     _keys,
     handle_dynamodb_errors,
@@ -37,6 +40,9 @@ from nrlf.core.types import DynamoDbClient
 from nrlf.producer.fhir.r4.tests.test_producer_nrlf_model import read_test_data
 
 from feature_tests.common.constants import DOCUMENT_POINTER_TABLE_DEFINITION
+from feature_tests.common.repository import FeatureTestRepository
+from helpers.aws_session import new_aws_session
+from helpers.terraform import get_terraform_json
 
 API_VERSION = 1
 INDEX_NAME = DOCUMENT_POINTER_TABLE_DEFINITION["GlobalSecondaryIndexes"][0]["IndexName"]
@@ -50,6 +56,14 @@ update_document_pointer_from_fhir_json = (
         *args, api_version=API_VERSION, **kwargs
     )
 )
+
+SUBJECT = "9278693472"
+
+
+@pytest.fixture(scope="session")
+def dynamodb_client():
+    session = new_aws_session()
+    return session.client("dynamodb")
 
 
 @handle_dynamodb_errors()
@@ -114,7 +128,7 @@ def test_read_document_pointer():
     with mock_dynamodb() as client:
         repository = Repository(item_type=DocumentPointer, client=client)
         repository.create(item=core_model)
-        result = repository.read_item(pk=core_model.pk)
+        result = repository.read_item(pk=core_model.pk.__root__)
     assert core_model == result
 
 
@@ -149,7 +163,7 @@ def test_update_document_pointer_success():
         repository = Repository(item_type=DocumentPointer, client=client)
         repository.create(item=model_1)
         repository.update(item=model_2)
-        item = repository.read_item(model_1.pk)
+        item = repository.read_item(model_1.pk.__root__)
 
     doc = json.loads(item.document.__root__)
     assert item.created_on.__root__ != model_1.created_on.__root__
@@ -190,10 +204,10 @@ def test_supersede_creates_new_item_and_deletes_existing():
             delete_pks=[model_1.pk],
         )
 
-        repository.read_item(model_2.pk)
+        repository.read_item(model_2.pk.__root__)
         try:
-            repository.read_item(model_1.pk)
-        except ItemNotFound as error:
+            repository.read_item(model_1.pk.__root__)
+        except ItemNotFound:
             pass
 
 
@@ -255,8 +269,8 @@ def test_hard_delete():
     with pytest.raises(ItemNotFound), mock_dynamodb() as client:
         repository = Repository(item_type=DocumentPointer, client=client)
         repository.create(item=model)
-        repository.hard_delete(model.pk)
-        repository.read_item(model.pk)
+        repository.hard_delete(model.pk.__root__)
+        repository.read_item(model.pk.__root__)
 
 
 def test_wont_hard_delete_if_item_doesnt_exist():
@@ -298,14 +312,14 @@ def test_search_returns_multiple_values_with_same_nhs_number():
         repository = Repository(item_type=DocumentPointer, client=client)
         repository.create(item=model_1)
         repository.create(item=model_2)
-        item = repository.query_gsi_1(model_1.pk_1)
+        item = repository.query_gsi_1(model_1.pk_1.__root__)
 
     assert len(item.document_pointers) == 2
 
 
 def test_search_returns_correct_values():
     doc_1 = generate_test_document_reference(
-        provider_doc_id="FIRST", subject=generate_test_subject("9278693472")
+        provider_doc_id="FIRST", subject=generate_test_subject(SUBJECT)
     )
     doc_2 = generate_test_document_reference(
         provider_doc_id="SECOND", subject=generate_test_subject("3137554160")
@@ -318,34 +332,146 @@ def test_search_returns_correct_values():
         repository.create(item=model_2)
 
         docs_gsi_1_response = repository.query_gsi_1(
-            model_1.pk_1
+            model_1.pk_1.__root__
         )  # NHS Number / Subject
         docs_gsi_2_response = repository.query_gsi_2(
-            model_1.pk_2
+            model_1.pk_2.__root__
         )  # ODS Code / Custodian
 
     assert len(docs_gsi_1_response.document_pointers) == 1, "Partitioned by subject"
     assert len(docs_gsi_2_response.document_pointers) == 2, "Partitioned by provider"
 
 
-def test_query_last_evaluated_key_is_returned_in_correct_circumstance():
-    doc_1 = generate_test_document_reference(
-        provider_doc_id="FIRST", subject=generate_test_subject("9278693472")
-    )
-    doc_2 = generate_test_document_reference(
-        provider_doc_id="SECOND", subject=generate_test_subject("9278693472")
-    )
-    model_1 = create_document_pointer_from_fhir_json(fhir_json=doc_1)
-    model_2 = create_document_pointer_from_fhir_json(fhir_json=doc_2)
-    with mock_dynamodb() as client:
-        repository = Repository(item_type=DocumentPointer, client=client)
-        repository.create(item=model_1)
-        repository.create(item=model_2)
+def _create_items(
+    doc_ids: list[str], repository: Repository, subject: str, custodian: str
+) -> list[DocumentPointer]:
+    items: list[DocumentPointer] = []
+    for doc_id in doc_ids:
+        doc = generate_test_document_reference(
+            provider_doc_id=doc_id,
+            subject=generate_test_subject(subject),
+            custodian={"identifier": {"value": custodian}},
+        )
+        item = create_document_pointer_from_fhir_json(fhir_json=doc)
+        repository.create(item=item)
+        items.append(item)
 
-        response = repository.query_gsi_1(model_1.pk_1, limit=1)  # NHS Number / Subject
-        response2 = repository.query_gsi_1(
-            model_1.pk_1, limit=2
-        )  # NHS Number / Subject
+    return items
 
-    assert response.last_evaluated_key is not None
-    assert response2.last_evaluated_key is None
+
+def _document_pointer_collection_are_same(
+    a: list[DocumentPointer], b: list[DocumentPointer]
+):
+    return sorted(a, key=lambda doc: doc.pk.__root__) == sorted(
+        b, key=lambda doc: doc.pk.__root__
+    )
+
+
+def _create_items_in_alternate_batches(
+    a_batch_sizes: list[int],
+    b_batch_sizes: list[int],
+    repository,
+    a_custodian,
+    b_custodian,
+):
+    a_batch_sizes = a_batch_sizes.copy()  # destructive operation below, so need to copy
+    b_batch_sizes = b_batch_sizes.copy()
+    a_docs, b_docs = [], []
+    while a_batch_sizes or b_batch_sizes:
+        if a_batch_sizes:
+            start, end = len(a_docs), len(a_docs) + a_batch_sizes.pop(-1)
+            a_docs += _create_items(
+                doc_ids=list(map(str, range(start, end))),
+                repository=repository,
+                subject=SUBJECT,
+                custodian=a_custodian,
+            )
+        if b_batch_sizes:
+            start, end = len(b_docs), len(b_docs) + b_batch_sizes.pop(-1)
+            b_docs += _create_items(
+                doc_ids=list(map(str, range(start, end))),
+                repository=repository,
+                subject=SUBJECT,
+                custodian=b_custodian,
+            )
+    return a_docs, b_docs
+
+
+BATCH_SIZES = (
+    [],
+    [0, PAGE_ITEM_LIMIT + 3, 4, 3 * PAGE_ITEM_LIMIT],
+    [PAGE_ITEM_LIMIT - 2, 0, 0, PAGE_ITEM_LIMIT + 3, 3],
+)
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("foo_batch_sizes", BATCH_SIZES)
+@pytest.mark.parametrize("bar_batch_sizes", BATCH_SIZES)
+def test_scroll_gsi_1(bar_batch_sizes, foo_batch_sizes, dynamodb_client):
+    """
+    Explanation:
+        * dynamodb applies filters results post-query *per page*, so it is possible for the following scenario:
+            * a given PK (e.g. subject always equals "123")
+            * multiple possible values of a filter (e.g. custodian="foo" or "bar")
+            * a globally unique sort key which is independent of the filter (e.g. a created date)
+         that the following effect can be observed when supplying a filter:
+            a) get fewer results than PAGE_SIZE *per page* from a single query that should in total
+               yield at least PAGE_SIZE results
+            b) have more pages of results even if the current page is empty
+            c) have more pages of results where some or all of those pages are empty
+        * the above scenario applies to at least GSI One
+    """
+
+    total_expected_docs = sum(foo_batch_sizes)
+    # NB: zero docs still means one page, hence 'or 1'
+    n_expected_pages = ceil(total_expected_docs / PAGE_ITEM_LIMIT) or 1
+    total_other_docs = sum(bar_batch_sizes)
+
+    repository = FeatureTestRepository(
+        item_type=DocumentPointer,
+        client=dynamodb_client,
+        environment_prefix=f'{get_terraform_json()["prefix"]["value"]}--',
+    )
+
+    # Create docs in alternate batches (since created data will therefore alternate)
+    # and validate that we've created what we expect
+    foo_docs, bar_docs = _create_items_in_alternate_batches(
+        a_batch_sizes=foo_batch_sizes,
+        a_custodian="foo",
+        b_batch_sizes=bar_batch_sizes,
+        b_custodian="bar",
+        repository=repository,
+    )
+    assert total_expected_docs == len(foo_docs)
+    assert total_other_docs == len(bar_docs)
+
+    # Scroll to get all results by GSI One for filter "foo"
+    pages = []
+    last_evaluated_key = None
+    last_evaluated_keys = []
+    for _ in range(n_expected_pages + 10):  # allow to scroll 10 more than anticipated
+        response = repository.query_gsi_1(
+            pk=f"P#{SUBJECT}", producer_id="foo", exclusive_start_key=last_evaluated_key
+        )
+        last_evaluated_key = response.last_evaluated_key
+
+        pages.append(response.document_pointers)
+        if not last_evaluated_key:
+            break
+        else:
+            last_evaluated_keys.append(last_evaluated_key)
+
+    # Test that all pages have size PAGE_ITEM_LIMIT, except the final page
+    assert len(pages) == n_expected_pages
+    if len(pages) > 1:
+        assert all(len(page) == PAGE_ITEM_LIMIT for page in pages[:-1])
+    if len(pages) > 0:
+        assert len(last_evaluated_keys) == len(pages) - 1
+        assert len(pages[-1]) <= PAGE_ITEM_LIMIT
+
+    # Test that we have retrieved all the expected documents
+    retrieved_items = list(chain.from_iterable(pages))  # Flatten list of lists
+    n_unique_items = len(set(item.pk.__root__ for item in retrieved_items))  # dupes
+    assert n_unique_items == len(retrieved_items) == total_expected_docs
+    assert _document_pointer_collection_are_same(a=retrieved_items, b=foo_docs)
+    assert last_evaluated_key is None
