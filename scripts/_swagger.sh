bce6e6bedea7f40ba3cbd1a4f6edd1d2ce6d6808
Brian Bayes <brian.bayes@live.co.uk>
2023-04-04 08:46:34 +0100

    [NRLF-XXXX] Remove swagger generate commands from nrlf.sh to avoid swagger issues

--- scripts/_swagger.sh
+++ scripts/_swagger.sh
@@ -6,8 +6,6 @@ function _swagger_help() {
     echo
     echo "commands:"
     echo "  help                         - this help screen"
-    echo "  generate <options>           - generate all swagger and models or producer/consumer if specified"
-    echo "  generate-swagger <options>   - generate all swagger or producer/consumer if specified"
     echo "  generate-model <options>     - generate all models or producer/consumer if specified"
     echo "  merge <options>              - generates the record-locator/<type>.yml file"
     echo
@@ -62,63 +60,6 @@ function _swagger() {
     local command=$1
     local type=$2
     case $command in
-    "generate")
-        if [[ -z "$type" ]]; then
-            _generate_from_fhir
-            (
-                cd scripts
-                python3 -c "import swagger_generator as sg; sg.entry()"
-            )
-            _generate_consumer_model
-            _generate_producer_model
-        else
-            _get_generator
-            if [ $type = 'consumer' ]; then
-                _generate_consumer_from_fhir
-                (
-                    cd scripts
-                    python3 -c "import swagger_generator as sg; sg.entry('$type')"
-                )
-                _generate_consumer_model
-            elif [ $type = 'producer' ]; then
-                _generate_producer_from_fhir
-                (
-                    cd scripts
-                    python3 -c "import swagger_generator as sg; sg.entry('$type')"
-                )
-                _generate_producer_model
-            else
-                rm -rf ./tools/
-                _nrlf_commands_help && return 1
-            fi
-            rm -rf ./tools/
-        fi
-        ;;
-    "generate-swagger")
-        if [[ -z "$type" ]]; then
-            _generate_from_fhir
-            (
-                cd scripts
-                python3 -c "import swagger_generator as sg; sg.entry()"
-            )
-        else
-            _get_generator
-            if [ $type = 'consumer' ]; then
-                _generate_consumer_from_fhir
-            elif [ $type = 'producer' ]; then
-                _generate_producer_from_fhir
-            else
-                rm -rf ./tools/
-                _nrlf_commands_help && return 1
-            fi
-            rm -rf ./tools/
-
-            (
-                cd scripts
-                python3 -c "import swagger_generator as sg; sg.entry('$type')"
-            )
-        fi
-        ;;
     "generate-model")
         if [[ -z "$type" ]]; then
             _generate_consumer_model
