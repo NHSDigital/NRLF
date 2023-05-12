22420c61f71c2312891737e0b5926adeeda8318a
Joel Klinger <joel.klinger1@nhs.net>
2023-05-12 16:23:06 +0100

    [NRLF-490] Defaults for unused logger fields

--- cron/seed_sandbox/config.py
+++ cron/seed_sandbox/config.py
@@ -8,8 +8,8 @@ class Config(BaseModel):
     AWS_REGION: str
     PREFIX: str
     ENVIRONMENT: str
-    SPLUNK_INDEX: str
-    SOURCE: str
+    SPLUNK_INDEX: str = "__splunk_index__"
+    SOURCE: str = "__source__"
 
 
 def build_persistent_dependencies(config: Config) -> dict[str, any]:
