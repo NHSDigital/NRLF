b9755e04a02ae5d9ecf752308d947dc8c75e2ba6
Rowan Gill <rowan.gill@burendo.com>
2023-06-06 16:54:57 +0100

    feautre/NRLF-505-rds-instance test vpc snook in

--- terraform/account-wide-infrastructure/test/rds-cluster-vpc.tf
+++ terraform/account-wide-infrastructure/test/rds-cluster-vpc.tf
@@ -4,15 +4,15 @@ data "aws_availability_zones" "available" {}
 # VPC
 #------------------------------------------------------------------------------
 
-resource "aws_vpc" "rds-cluster-vpc-test" {
+resource "aws_vpc" "rds-cluster-vpc-dev" {
   cidr_block           = var.vpc_cidr_block
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name        = "nhsd-nrlf-rds-cluster-vpc-test"
-    Environment = "test"
+    Name        = "nhsd-nrlf-rds-cluster-vpc-dev"
+    Environment = "dev"
   }
 }
 
@@ -20,26 +20,26 @@ resource "aws_vpc" "rds-cluster-vpc-test" {
 # Subnets
 #------------------------------------------------------------------------------
 
-resource "aws_subnet" "private_subnet-test" {
+resource "aws_subnet" "private_subnet-dev" {
   count             = var.subnet_count.private
-  vpc_id            = aws_vpc.rds-cluster-vpc-test.id
+  vpc_id            = aws_vpc.rds-cluster-vpc-dev.id
   cidr_block        = element(var.private_subnet_cidr_blocks, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name        = "nhsd-nrlf-private-subnet-${count.index}-test"
-    Environment = "test"
+    Name        = "nhsd-nrlf-private-subnet-${count.index}-dev"
+    Environment = "dev"
     Tier        = "private"
   }
 }
 
 
 resource "aws_db_subnet_group" "rds-cluster-subnet-group" {
-  name       = "nhsd-nrlf-private-subnet-group-test"
-  subnet_ids = aws_subnet.private_subnet-test.*.id
+  name       = "nhsd-nrlf-private-subnet-group-dev"
+  subnet_ids = aws_subnet.private_subnet-dev.*.id
 
   tags = {
-    Name = "nhsd-nrlf-private-subnet-group-test"
+    Name = "nhsd-nrlf-private-subnet-group-dev"
   }
 }
 
@@ -48,41 +48,12 @@ resource "aws_db_subnet_group" "rds-cluster-subnet-group" {
 # Route Table
 #------------------------------------------------------------------------------
 
-resource "aws_route_table" "private_route_table-test" {
-  vpc_id = aws_vpc.rds-cluster-vpc-test.id
+resource "aws_route_table" "private_route_table-dev" {
+  vpc_id = aws_vpc.rds-cluster-vpc-dev.id
 }
 
-resource "aws_route_table_association" "private-test" {
+resource "aws_route_table_association" "private-dev" {
   count          = var.subnet_count.private
-  subnet_id      = element(aws_subnet.private_subnet-test, count.index).id
-  route_table_id = aws_route_table.private_route_table-test.id
-}
-
-#------------------------------------------------------------------------------
-# Security Group
-#------------------------------------------------------------------------------
-
-resource "aws_security_group" "rds-cluster-vpc-sg" {
-  name        = "nhsd-nrlf-rds-cluster-sg-test"
-  description = "Security group for nhsd-nrlf-rds-cluster-vpc"
-  vpc_id      = aws_vpc.rds-cluster-vpc-test.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name        = "nhsd-nrlf-rds-cluster-sg-test"
-    Environment = "test"
-  }
+  subnet_id      = element(aws_subnet.private_subnet-dev, count.index).id
+  route_table_id = aws_route_table.private_route_table-dev.id
 }
