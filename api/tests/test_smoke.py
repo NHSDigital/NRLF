766f5bf9ed50ff590d261d49b5315d6f8846df46
Joel Klinger <joel.klinger1@nhs.net>
2023-06-07 14:36:03 +0100

    [feature/NRLF-517-nrl_sync_smoke_test] NRL sync smoke tests

--- api/tests/test_smoke.py
+++ api/tests/test_smoke.py
@@ -1,113 +1,18 @@
-import base64
 import os
 import re
-import time
 import urllib.parse
-from datetime import datetime
-from functools import cache
 from uuid import uuid4
 
-import boto3
-import botocore.session
 import fire
-import jwt
 import pytest
 import requests
 
-from helpers.aws_session import (
-    AWS_ACCOUNT_FOR_ENV,
-    DEFAULT_WORKSPACE,
-    aws_account_id_from_profile,
-)
-from helpers.terraform import get_terraform_json
-from nrlf.core.validators import json_loads
-
-DEFAULT_WORKSPACE = "dev"
+from helpers.aws_session import DEFAULT_WORKSPACE
+from helpers.oauth import APIGEE_ENV_FOR_ENV, DEFAULT_APP_ALIAS, token
 
 ENV_NAME_RE = re.compile("^(?P<env>\w+)(-(?P<sandbox>sandbox)?)?$")
 
-
-# Map an environment to an AWS Account
-AWS_ACCOUNT_FOR_ENV = {
-    DEFAULT_WORKSPACE: "dev",
-    "int": "test",
-    "ref": "test",
-    "prod": "prod",
-}
-
-APIGEE_PROXY_FOR_ENV = {
-    "dev": "internal-dev",
-    "dev-sandbox": "internal-dev-sandbox",
-    "ref": "internal-qa",
-    "ref-sandbox": "internal-qa-sandbox",
-    "int": "int",
-    "int-sandbox": "sandbox",
-    "prod": "",
-}
-
 APIGEE_BASE_URL = "api.service.nhs.uk"
-
-
-def generate_jwt(
-    jwt_private_key: str,
-    oauth_url: str,
-    api_key: str,
-    nonce: str,
-    expires: time.time,
-    kid: str,
-):
-    payload = {
-        "iss": api_key,
-        "sub": api_key,
-        "aud": oauth_url,
-        "jti": nonce,
-        "exp": expires,
-    }
-    return jwt.encode(payload, jwt_private_key, algorithm="RS512", headers={"kid": kid})
-
-
-def oauth_access_token(api_key, jwt_private_key, oauth_url, kid):
-    nonce = f"correlation-id-{uuid4()}"
-    key = jwt_private_key
-
-    jwt_token = generate_jwt(
-        jwt_private_key=key,
-        oauth_url=oauth_url,
-        api_key=api_key,
-        nonce=nonce,
-        expires=time.time() + 30,
-        kid=kid,
-    )
-
-    response = requests.post(
-        url=oauth_url,
-        headers={"content-type": "application/x-www-form-urlencoded"},
-        data={
-            "grant_type": "client_credentials",
-            "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-            "client_assertion": jwt_token,
-        },
-    )
-    if response.status_code != 200:
-        return False, response.text
-    assert response.status_code == 200, response.text
-    oauth_token = json_loads(response.text)["access_token"]
-    return True, oauth_token
-
-
-def aws_account_id_from_profile(env: str):
-    account = AWS_ACCOUNT_FOR_ENV[env]
-    profile_names = [f"nhsd-nrlf-{account}-admin", f"nhsd-nrlf-{account}"]
-
-    session = botocore.session.Session()
-    profiles = session.full_config["profiles"]
-
-    for name in profile_names:
-        account_id = profiles.get(name, {}).get("aws_account_id")
-        if account_id:
-            return account_id
-
-    raise Exception("No valid profile found")
 
 
 def aws_read_ssm_apigee_proxy(session, env: str):
@@ -120,64 +25,9 @@ def aws_read_ssm_apigee_proxy(session, env: str):
     return param["Parameter"]["Value"]
 
 
-def aws_read_apigee_app_secrets(session, secret_id: str):
-    secrets_client = session.client("secretsmanager")
-
-    response = secrets_client.get_secret_value(SecretId=secret_id)
-    secret = json_loads(response["SecretString"])
-
-    api_key = secret["api_key"]
-    oauth_url = secret["oauth_url"]
-    kid = secret["kid"]
-    private_key = base64.b64decode(secret["private_key"])
-    # Check other required fields are present
-    for field in ["apigee_url", "public_key"]:
-        secret[field]  # Will raise KeyError if missing
-
-    return api_key, oauth_url, kid, private_key
-
-
-def aws_session_assume_terraform_role(account_id):
-    sts_client = boto3.client("sts")
-
-    assumed_role = sts_client.assume_role(
-        RoleArn=f"arn:aws:iam::{account_id}:role/terraform",
-        RoleSessionName=f"smoke-test-{datetime.utcnow().timestamp()}",
-    )
-    session = boto3.Session(
-        aws_access_key_id=assumed_role["Credentials"]["AccessKeyId"],
-        aws_secret_access_key=assumed_role["Credentials"]["SecretAccessKey"],
-        aws_session_token=assumed_role["Credentials"]["SessionToken"],
-    )
-
-    return session
-
-
-@cache
-def get_oauth_token(session, account: str, env: str):
-    secret_name = f"nhsd-nrlf--{account}--{env}--apigee-app--smoke-test"
-    try:
-        api_key, oauth_url, kid, private_key = aws_read_apigee_app_secrets(
-            session, secret_name
-        )
-    except Exception as e:
-        raise Exception(f"Cannot find secret {secret_name}") from e
-
-    success, oauth_token = oauth_access_token(api_key, private_key, oauth_url, kid)
-    assert success, oauth_token
-
-    return oauth_token
-
-
-def create_apigee_url(env: str, actor: str, sandbox: str):
-    if sandbox:
-        env = f"{env}-{sandbox}"
-    apigee_env = APIGEE_PROXY_FOR_ENV[env]
-
-    if apigee_env == "":
-        return f"https://{APIGEE_BASE_URL}/record-locator/{actor}"
-
-    return f"https://{apigee_env}.{APIGEE_BASE_URL}/record-locator/{actor}"
+def create_apigee_url(apigee_env: str, actor: str):
+    base_url = ".".join(filter(bool, (apigee_env, APIGEE_BASE_URL)))
+    return f"https://{base_url}/record-locator/{actor}"
 
 
 def generate_end_user_header(env):
@@ -192,18 +42,11 @@ def generate_end_user_receiver_header(env):
     return "DEF"
 
 
-def _prepare_base_request(env: str, actor: str, sandbox: str) -> tuple[str, dict]:
-    if os.environ.get("RUNNING_IN_CI"):
-        account_id = get_terraform_json()["assume_account_id"]["value"]
-    else:
-        account_id = aws_account_id_from_profile(env)
-    account = AWS_ACCOUNT_FOR_ENV[env]
-
-    session = aws_session_assume_terraform_role(account_id)
-
-    oauth_token = get_oauth_token(session, account, env)
-
-    base_url = create_apigee_url(env, actor, sandbox)
+def _prepare_base_request(env: str, actor: str, app_alias: str) -> tuple[str, dict]:
+    base_env = split_env_variable(env)["env"]
+    oauth_token = token(env=base_env, app_alias=app_alias)
+    apigee_env = APIGEE_ENV_FOR_ENV[env]
+    base_url = create_apigee_url(apigee_env=apigee_env, actor=actor)
     headers = {
         "accept": "application/json; version=1.0",
         "authorization": f"Bearer {oauth_token}",
@@ -220,7 +63,6 @@ def environment() -> str:
 
 
 def split_env_variable(environment):
-
     result = ENV_NAME_RE.match(environment)
     if not result or environment.endswith("-"):
         raise ValueError(f"'{environment}' must be of the form 'env' or 'env-sandbox'")
@@ -228,11 +70,13 @@ def split_env_variable(environment):
 
 
 class Smoketests:
-    def manual_smoke_test(self, actor, environment: str):
+    def manual_smoke_test(
+        self, actor, environment: str, app_alias: str = DEFAULT_APP_ALIAS
+    ):
         print("🏃 Running 🏃 smoke test - 🤔")  # noqa: T201
-
-        env_kwargs = split_env_variable(environment)
-        base_url, headers = _prepare_base_request(**env_kwargs, actor=actor)
+        base_url, headers = _prepare_base_request(
+            env=environment, actor=actor, app_alias=app_alias
+        )
         patient_id = urllib.parse.quote(f"https://fhir.nhs.uk/Id/nhs-number|9278693472")
         url = f"{base_url}/FHIR/R4/DocumentReference?subject:identifier={patient_id}"
         headers["NHSD-End-User-Organisation-ODS"] = generate_end_user_header(
@@ -246,11 +90,6 @@ class Smoketests:
             print("🎉🎉 - Your 💨 Smoke 💨 test has passed - 🎉🎉")  # noqa: T201
         else:
             raise fire.core.FireError("The smoke test has failed 😭😭")
-
-    def token(self, env, account):
-        account_id = aws_account_id_from_profile(env)
-        session = aws_session_assume_terraform_role(account_id)
-        print(get_oauth_token(session, env=env, account=account))  # noqa: T201
 
 
 if __name__ == "__main__":
---
+++ helpers/helpers/oauth.py
@@ -0,0 +1,151 @@
+import base64
+import os
+import time
+from functools import cache
+from typing import Literal
+from uuid import uuid4
+
+import fire
+import jwt
+import requests
+
+from helpers.aws_session import (
+    AWS_ACCOUNT_FOR_ENV,
+    aws_account_id_from_profile,
+    new_aws_session,
+)
+from helpers.log import log
+from helpers.terraform import get_terraform_json
+from nrlf.core.validators import json_loads
+
+EXPIRATION_TIME_SECONDS = 30
+
+_DEFAULT_APP_DEV = "4e41d2d9-3ef6-48dc-8406-5faba77ffd83"
+_DEFAULT_APP_PROD = "7a4ad8c7-3927-4259-af7b-ac9d305a86ad"
+_DEFAULT_APP_FOR_APIGEE_ENV = {
+    "internal-dev": _DEFAULT_APP_DEV,
+    "internal-dev-sandbox": _DEFAULT_APP_DEV,
+    "internal-qa": _DEFAULT_APP_DEV,
+    "internal-qa-sandbox": _DEFAULT_APP_DEV,
+    "int": _DEFAULT_APP_PROD,
+    "int-sandbox": _DEFAULT_APP_PROD,
+    "prod": _DEFAULT_APP_PROD,
+}
+
+_NRL_SYNC_APP_DEV = "94492ef7-40a5-47bb-a18b-b9023bd4ec9a"
+_NRL_SYNC_APP_PROD = "f9822096-11ec-479f-8d95-fcc8dd22048d"
+_NRL_SYNC_APP_FOR_APIGEE_ENV = {
+    "internal-dev": _NRL_SYNC_APP_DEV,
+    "internal-qa": _NRL_SYNC_APP_DEV,
+    "int": _NRL_SYNC_APP_PROD,
+    "prod": _NRL_SYNC_APP_PROD,
+}
+
+DEFAULT_APP_ALIAS = "default"
+APP_FOR_ALIAS = {
+    DEFAULT_APP_ALIAS: _DEFAULT_APP_FOR_APIGEE_ENV,
+    "nrl_sync": _NRL_SYNC_APP_FOR_APIGEE_ENV,
+}
+
+APIGEE_ENV_FOR_ENV = {
+    "dev": "internal-dev",
+    "dev-sandbox": "internal-dev-sandbox",
+    "ref": "internal-qa",
+    "ref-sandbox": "internal-qa-sandbox",
+    "int": "int",
+    "int-sandbox": "sandbox",
+    "prod": "prod",
+}
+
+
+@log("Generating jwt")
+def _generate_jwt(
+    jwt_private_key: str,
+    oauth_url: str,
+    api_key: str,
+    uuid: str,
+    expires: time.time,
+    kid: str,
+):
+    payload = {
+        "iss": api_key,
+        "sub": api_key,
+        "aud": oauth_url,
+        "jti": uuid,
+        "exp": expires,
+    }
+    return jwt.encode(payload, jwt_private_key, algorithm="RS512", headers={"kid": kid})
+
+
+@log("Requesting OAuth token from '{oauth_url}'")
+def _oauth_access_token(jwt_token: str, oauth_url: str):
+    response = requests.post(
+        url=oauth_url,
+        headers={"content-type": "application/x-www-form-urlencoded"},
+        data={
+            "grant_type": "client_credentials",
+            "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+            "client_assertion": jwt_token,
+        },
+    )
+    response.raise_for_status()
+    return json_loads(response.text)["access_token"]
+
+
+@log("Reading secret '{secret_id}'")
+def _aws_read_apigee_app_secrets(session, secret_id: str):
+    secrets_client = session.client("secretsmanager")
+
+    response = secrets_client.get_secret_value(SecretId=secret_id)
+    secret = json_loads(response["SecretString"])
+
+    api_key = secret["api_key"]
+    oauth_url = secret["oauth_url"]
+    kid = secret["kid"]
+    private_key = base64.b64decode(secret["private_key"])
+    # Check other required fields are present
+    for field in ["apigee_url", "public_key"]:
+        secret[field]  # Will raise KeyError if missing
+
+    return api_key, oauth_url, kid, private_key
+
+
+@log("Got OAuth token: {__result__}")
+@cache
+def get_oauth_token(session, account: str, env: str, app_id: str):
+    secret_id = f"nhsd-nrlf--{account}--{env}--apigee-app-token-info--{app_id}"
+    try:
+        api_key, oauth_url, kid, jwt_private_key = _aws_read_apigee_app_secrets(
+            session=session, secret_id=secret_id
+        )
+    except Exception as e:
+        raise Exception(f"Cannot find secret {secret_id}") from e
+
+    jwt_token = _generate_jwt(
+        jwt_private_key=jwt_private_key,
+        oauth_url=oauth_url,
+        api_key=api_key,
+        uuid=f"correlation-id-{uuid4()}",
+        expires=time.time() + EXPIRATION_TIME_SECONDS,
+        kid=kid,
+    )
+
+    return _oauth_access_token(jwt_token=jwt_token, oauth_url=oauth_url)
+
+
+def token(env: str, app_alias: Literal["default", "nrl_sync"] = DEFAULT_APP_ALIAS):
+    account = AWS_ACCOUNT_FOR_ENV[env]
+    apigee_env = APIGEE_ENV_FOR_ENV[env]
+    app_id = APP_FOR_ALIAS[app_alias][apigee_env]
+
+    if os.environ.get("RUNNING_IN_CI"):
+        account_id = get_terraform_json()["assume_account_id"]["value"]
+    else:
+        account_id = aws_account_id_from_profile(env=env)
+
+    session = new_aws_session(account_id=account_id)
+    return get_oauth_token(session=session, env=env, account=account, app_id=app_id)
+
+
+if __name__ == "__main__":
+    fire.Fire(token)
