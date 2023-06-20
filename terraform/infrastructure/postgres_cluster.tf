b51e88c79226aab3d901a8921c427dcdcd0fd4f9
Joel Klinger <joel.klinger1@nhs.net>
2023-06-20 15:46:08 +0100

    [feature/NRLF-506b-db_schema] Add schema

--- terraform/infrastructure/postgres_cluster.tf
+++ terraform/infrastructure/postgres_cluster.tf
@@ -1,12 +1,13 @@
 module "mi" {
-  source             = "./modules/postgres_cluster"
-  schema_path        = "../../mi/schema/schema.sql"
-  name               = "mi"
-  prefix             = local.prefix
-  environment        = local.environment
-  region             = local.region
-  cloudwatch_kms_arn = module.kms__cloudwatch.kms_arn
-  account_name       = local.account_name
-  layers             = [module.third_party.layer_arn]
-  assume_account     = var.assume_account
+  source                 = "./modules/postgres_cluster"
+  schema_path            = "../../mi/schema/schema.sql"
+  static_dimensions_path = "../../mi/schema/static_dimensions.sql"
+  name                   = "mi"
+  prefix                 = local.prefix
+  environment            = local.environment
+  region                 = local.region
+  cloudwatch_kms_arn     = module.kms__cloudwatch.kms_arn
+  account_name           = local.account_name
+  layers                 = [module.third_party.layer_arn]
+  assume_account         = var.assume_account
 }
