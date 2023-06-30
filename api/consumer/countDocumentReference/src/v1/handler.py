76d1816da498987403b58efb304bea4e7e2ab7de
andrewrae5-nhs <andrew.rae5@nhs.net>
2023-06-30 17:47:26 +0100

    refactored the count doc ref

--- api/consumer/countDocumentReference/src/v1/handler.py
+++ api/consumer/countDocumentReference/src/v1/handler.py
@@ -5,11 +5,11 @@ from typing import Any
 from aws_lambda_powertools.utilities.parser.models import APIGatewayProxyEventModel
 from lambda_pipeline.types import FrozenDict, LambdaContext, PipelineData
 
-from nrlf.consumer.fhir.r4.model import NextPageToken, RequestQuerySubject
+from layer.nrlf.nrlf.core.common_search_steps import get_paginated_document_references
+from nrlf.consumer.fhir.r4.model import NextPageToken
 from nrlf.core.common_steps import make_common_log_action, parse_headers
-from nrlf.core.errors import assert_no_extra_params
-from nrlf.core.model import CountRequestParams, PaginatedResponse, key
-from nrlf.core.repository import COUNT_ITEM_LIMIT, Repository, type_filter
+from nrlf.core.model import CountRequestParams, PaginatedResponse
+from nrlf.core.repository import COUNT_ITEM_LIMIT
 from nrlf.core.transform import create_bundle_count
 
 log_action = make_common_log_action()
@@ -27,32 +27,21 @@ def search_document_references(
     dependencies: FrozenDict[str, Any],
     logger: Logger,
 ) -> PipelineData:
-    repository: Repository = dependencies["repository"]
-
-    request_params = CountRequestParams(**event.queryStringParameters or {})
-
-    assert_no_extra_params(
-        request_params=request_params, provided_params=event.queryStringParameters
-    )
-
-    nhs_number: RequestQuerySubject = request_params.nhs_number
-
-    pointer_types = type_filter(
-        type_identifier=None,
-        pointer_types=data["pointer_types"],
-    )
-
-    pk = key("P", nhs_number)
 
     count = 0
     next_page_token: NextPageToken = None
+
+    request_params = CountRequestParams(**event.queryStringParameters or {})
+
     while True:
-        response: PaginatedResponse = repository.query_gsi_1(
-            pk=pk,
-            type=pointer_types,
-            nhs_number=nhs_number,
-            exclusive_start_key=next_page_token,
-            limit=COUNT_ITEM_LIMIT,
+        response: PaginatedResponse = get_paginated_document_references(
+            data,
+            request_params,
+            event.queryStringParameters,
+            dependencies,
+            logger,
+            COUNT_ITEM_LIMIT,
+            next_page_token,
         )
         count = count + len(response.document_pointers)
         next_page_token = response.last_evaluated_key
--- layer/nrlf/nrlf/core/common_search_steps.py
+++ layer/nrlf/nrlf/core/common_search_steps.py
@@ -1,15 +1,21 @@
 from enum import Enum
 from logging import Logger
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 from lambda_pipeline.types import FrozenDict, PipelineData
+from pydantic import Field
 from typing_extensions import Annotated
 
 from layer.nrlf.nrlf.core.constants import DbPrefix
 from nrlf.consumer.fhir.r4.model import NextPageToken, RequestQuerySubject
 from nrlf.core.common_steps import make_common_log_action
 from nrlf.core.errors import assert_no_extra_params
-from nrlf.core.model import ConsumerRequestParams, PaginatedResponse, key
+from nrlf.core.model import (
+    ConsumerRequestParams,
+    CountRequestParams,
+    PaginatedResponse,
+    key,
+)
 from nrlf.core.repository import (
     PAGE_ITEM_LIMIT,
     Repository,
@@ -28,7 +34,7 @@ class LogReference(Enum):
 @log_action(log_reference=LogReference.COMMONSEARCH001)
 def get_paginated_document_references(
     data: PipelineData,
-    requestParams: ConsumerRequestParams,
+    requestParams: Union[ConsumerRequestParams, CountRequestParams],
     queryStringParams: Dict[str, str],
     dependencies: FrozenDict[str, Any],
     logger: Logger,
@@ -47,23 +53,31 @@ def get_paginated_document_references(
 
     nhs_number: RequestQuerySubject = request_params.nhs_number
 
-    custodian = custodian_filter(
-        custodian_identifier=request_params.custodian_identifier
-    )
+    custodian = None
+    if isinstance(requestParams, ConsumerRequestParams):
+        custodian = custodian_filter(
+            custodian_identifier=request_params.custodian_identifier
+        )
+        pointer_types = type_filter(
+            type_identifier=request_params.type,
+            pointer_types=data["pointer_types"],
+        )
+        validate_type_system(request_params.type, pointer_types=data["pointer_types"])
+    else:
+        custodian = None
+        pointer_types = type_filter(
+            type_identifier=None,
+            pointer_types=data["pointer_types"],
+        )
 
-    validate_type_system(request_params.type, pointer_types=data["pointer_types"])
+    if isinstance(requestParams, ConsumerRequestParams):
+        if pageToken is None:
+            next_page_token: NextPageToken = request_params.next_page_token
+        if next_page_token is not None:
+            next_page_token = next_page_token.__root__
 
-    pointer_types = type_filter(
-        type_identifier=request_params.type,
-        pointer_types=data["pointer_types"],
-    )
-    if pageToken is None:
-        next_page_token: NextPageToken = request_params.next_page_token
     else:
         next_page_token: NextPageToken = pageToken
-
-    if next_page_token is not None:
-        next_page_token = next_page_token.__root__
 
     pk = key(DbPrefix.Patient, nhs_number)
 
