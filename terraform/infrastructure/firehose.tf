61d6927a6d03c500d662d949a5480d5429ee6ff3
eesa456 <eesa.mahmood1@nhs.net>
2023-07-19 11:20:59 +0100

    add deletetion preventioon to firehose s3 bucket

--- terraform/infrastructure/firehose.tf
+++ terraform/infrastructure/firehose.tf
@@ -1,12 +1,12 @@
 module "firehose__processor" {
-  source               = "./modules/firehose"
-  assume_account       = var.assume_account
-  prefix               = local.prefix
-  region               = local.region
-  layers               = [module.lambda-utils.layer_arn, module.nrlf.layer_arn, module.third_party.layer_arn]
-  environment          = local.environment
-  cloudwatch_kms_arn   = module.kms__cloudwatch.kms_arn
-  splunk_index         = local.splunk_index
-  destination          = contains(local.persistent_environments, local.environment) ? "splunk" : "extended_s3"
-  slack_alerts_enabled = contains(local.persistent_environments, local.environment)
+  source                    = "./modules/firehose"
+  assume_account            = var.assume_account
+  prefix                    = local.prefix
+  region                    = local.region
+  layers                    = [module.lambda-utils.layer_arn, module.nrlf.layer_arn, module.third_party.layer_arn]
+  environment               = local.environment
+  cloudwatch_kms_arn        = module.kms__cloudwatch.kms_arn
+  splunk_index              = local.splunk_index
+  destination               = contains(local.persistent_environments, local.environment) ? "splunk" : "extended_s3"
+  is_persistent_environment = contains(local.persistent_environments, local.environment)
 }
