dcc58baa69cab54cd55965ac24584b58bbcd0062
Chris Kemp <chris.kemp3@nhs.net>
2023-09-01 10:32:39 +0100

    added dupes

---
+++ foo.ts
@@ -0,0 +1,55 @@
+function foo() {
+  let s = "this";
+  s += " is";
+  s += " a";
+  s += " dupe";
+  return s;
+}
+
+function bar() {
+  let s = "this";
+  s += " is";
+  s += " a";
+  s += " dupe";
+  return s;
+}
+
+function bar() {
+  let s = "this";
+  s += " is";
+  s += " a";
+  s += " dupe";
+  return s;
+}
+
+function bar() {
+  let s = "this";
+  s += " is";
+  s += " a";
+  s += " dupe";
+  return s;
+}
+
+function bar() {
+  let s = "this";
+  s += " is";
+  s += " a";
+  s += " dupe";
+  return s;
+}
+
+function bar() {
+  let s = "this";
+  s += " is";
+  s += " a";
+  s += " dupe";
+  return s;
+}
+
+function bar() {
+  let s = "this";
+  s += " is";
+  s += " a";
+  s += " dupe";
+  return s;
+}
