771d06e556fee99ba7a2ea950d95f1607d6b2595
Hamza Khan <hamza.khan24@nhs.net>
2023-11-22 17:23:10 +0000

    update readme

--- scripts/_test.sh
+++ scripts/_test.sh
@@ -56,7 +56,7 @@ function _test_feature() {
   case $command in
   "local") python -m behave $args ;;
   "integration")
-    rm -rf ./allure-results
+    rm -rf ./allure-results &&
     python -m behave --define="integration_test=true" $args --alluredir=./allure-results
     allure generate ./allure-results -o ./allure-report --clean
     allure open ./allure-report ;;
