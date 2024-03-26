0aff4167ce44e1712635090db8dc17a2b15ac6a3
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2024-03-26 17:55:49 +0000

    NRL-578 Comment out erroring data resources

--- terraform/infrastructure/modules/postgres_cluster/vpc.tf
+++ terraform/infrastructure/modules/postgres_cluster/vpc.tf
@@ -1,21 +1,21 @@
-data "aws_vpc" "rds-cluster-vpc" {
-  tags = {
-    Name        = "nhsd-nrlf-rds-cluster-vpc-${var.account_name}"
-    Environment = var.account_name
-  }
-}
+# data "aws_vpc" "rds-cluster-vpc" {
+#   tags = {
+#     Name        = "nhsd-nrlf-rds-cluster-vpc-${var.account_name}"
+#     Environment = var.account_name
+#   }
+# }
 
-data "aws_subnets" "rds-cluster-vpc-private-subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.rds-cluster-vpc.id]
-  }
+# data "aws_subnets" "rds-cluster-vpc-private-subnets" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.rds-cluster-vpc.id]
+#   }
 
-  tags = {
-    Tier = "private"
-  }
-}
+#   tags = {
+#     Tier = "private"
+#   }
+# }
 
-data "aws_db_subnet_group" "rds-cluster-subnet-group" {
-  name = "nhsd-nrlf-private-subnet-group-${var.account_name}"
-}
+# data "aws_db_subnet_group" "rds-cluster-subnet-group" {
+#   name = "nhsd-nrlf-private-subnet-group-${var.account_name}"
+# }
