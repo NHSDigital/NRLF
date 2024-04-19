95420cb094475ff98d4902357d18ec9621be20cb
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2024-04-19 13:02:30 +0100

    NRL-661 Cleanup unused OIDC providers

--- terraform/account-wide-infrastructure/test/oidc.tf
+++
@@ -1,9 +0,0 @@
-data "tls_certificate" "github_tls_certificate" {
-  url = "https://token.actions.githubusercontent.com"
-}
-
-resource "aws_iam_openid_connect_provider" "github_action" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = data.tls_certificate.github_tls_certificate.certificates[*].sha1_fingerprint
-}
