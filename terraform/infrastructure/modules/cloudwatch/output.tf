d29618e11f14dbefad5bc3baaaaca102180ab194
eesa456 <eesa.mahmood1@nhs.net>
2024-07-10 14:27:16 +0100

    NRL-693 make arn for first instance

--- terraform/infrastructure/modules/cloudwatch/output.tf
+++ terraform/infrastructure/modules/cloudwatch/output.tf
@@ -1,4 +1,4 @@
 output "cloudwatch_metric_alarm_arn" {
   description = "The ARN of the Cloudwatch metric alarm."
-  value       = try(aws_cloudwatch_metric_alarm.metric_alarm.arn, "")
+  value       = try(aws_cloudwatch_metric_alarm.metric_alarm[0].arn, "")
 }
