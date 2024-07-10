5da35a61829e09d8a7ce3bc0755e0366d9ebadfa
eesa456 <eesa.mahmood1@nhs.net>
2024-07-10 14:49:17 +0100

    NRL-693 fix email

--- terraform/infrastructure/modules/sns/sns.tf
+++ terraform/infrastructure/modules/sns/sns.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "sns_topic" {
 }
 
 resource "aws_sns_topic_subscription" "sns_subscription" {
-  for_each  = toset(["spine-cell-sigma-noti-aaaalor2u6funj7q3a4v7cpuba@nhsdigitalcorporate.org.slack.com", "eesa,mahmood1@nhs.net"])
+  for_each  = toset(["spine-cell-sigma-noti-aaaalor2u6funj7q3a4v7cpuba@nhsdigitalcorporate.org.slack.com", "eesa.mahmood1@nhs.net"])
   topic_arn = aws_sns_topic.sns_topic.arn
   protocol  = var.protocol
   endpoint  = each.value
