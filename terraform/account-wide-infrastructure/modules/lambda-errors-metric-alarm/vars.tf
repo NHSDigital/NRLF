0a0f7bcb06de0bb84462227fddf9ea6400d6ce6d
eesa456 <eesa.mahmood1@nhs.net>
2024-07-11 16:53:20 +0100

    NRL-693 remove useless vars

--- terraform/account-wide-infrastructure/modules/lambda-errors-metric-alarm/vars.tf
+++ terraform/account-wide-infrastructure/modules/lambda-errors-metric-alarm/vars.tf
@@ -20,12 +20,6 @@ variable "threshold" {
   default     = null
 }
 
-variable "threshold_metric_id" {
-  description = "If this is an alarm based on an anomaly detection model, make this value match the ID of the ANOMALY_DETECTION_BAND function."
-  type        = string
-  default     = null
-}
-
 variable "unit" {
   description = "The unit for the alarm's associated metric."
   type        = string
@@ -54,72 +48,6 @@ variable "statistic" {
   description = "The statistic to apply to the alarm's associated metric. Either of the following is supported: SampleCount, Average, Sum, Minimum, Maximum"
   type        = string
   default     = null
-}
-
-variable "actions_enabled" {
-  description = "Indicates whether or not actions should be executed during any changes to the alarm's state. Defaults to true."
-  type        = bool
-  default     = true
-}
-
-variable "datapoints_to_alarm" {
-  description = "The number of datapoints that must be breaching to trigger the alarm."
-  type        = number
-  default     = null
-}
-
-variable "dimensions" {
-  description = "The dimensions for the alarm's associated metric."
-  type        = any
-  default     = null
-}
-
-variable "alarm_actions" {
-  description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state. Each action is specified as an Amazon Resource Name (ARN)."
-  type        = list(string)
-  default     = null
-}
-
-variable "insufficient_data_actions" {
-  description = "The list of actions to execute when this alarm transitions into an INSUFFICIENT_DATA state from any other state. Each action is specified as an Amazon Resource Name (ARN)."
-  type        = list(string)
-  default     = null
-}
-
-variable "ok_actions" {
-  description = "The list of actions to execute when this alarm transitions into an OK state from any other state. Each action is specified as an Amazon Resource Name (ARN)."
-  type        = list(string)
-  default     = null
-}
-
-variable "extended_statistic" {
-  description = "The percentile statistic for the metric associated with the alarm. Specify a value between p0.0 and p100."
-  type        = string
-  default     = null
-}
-
-variable "treat_missing_data" {
-  description = "Sets how this alarm is to handle missing data points. The following values are supported: missing, ignore, breaching and notBreaching."
-  type        = string
-  default     = "missing"
-}
-
-variable "evaluate_low_sample_count_percentiles" {
-  description = "Used only for alarms based on percentiles. If you specify ignore, the alarm state will not change during periods with too few data points to be statistically significant. If you specify evaluate or omit this parameter, the alarm will always be evaluated and possibly change state no matter how many data points are available. The following values are supported: ignore, and evaluate."
-  type        = string
-  default     = null
-}
-
-variable "metric_query" {
-  description = "Enables you to create an alarm based on a metric math expression. You may specify at most 20."
-  type        = any
-  default     = []
-}
-
-variable "tags" {
-  description = "A mapping of tags to assign to all resources"
-  type        = map(string)
-  default     = {}
 }
 
 variable "name_prefix" {
