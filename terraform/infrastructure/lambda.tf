32729754ee09dbe2022f166af37deab90d564143
eesa456 <eesa.mahmood1@nhs.net>
2024-07-24 16:09:26 +0100

    NRL-756 change retention day to 7 for ephemeral

--- terraform/infrastructure/lambda.tf
+++ terraform/infrastructure/lambda.tf
@@ -24,7 +24,7 @@ module "consumer__readDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "read_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "consumer__countDocumentReference" {
@@ -53,7 +53,7 @@ module "consumer__countDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "count_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 
 }
 
@@ -83,7 +83,7 @@ module "consumer__searchDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "search_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "consumer__searchPostDocumentReference" {
@@ -112,7 +112,7 @@ module "consumer__searchPostDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "search_post_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "producer__createDocumentReference" {
@@ -142,7 +142,7 @@ module "producer__createDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "create_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "producer__deleteDocumentReference" {
@@ -172,7 +172,7 @@ module "producer__deleteDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "delete_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "producer__readDocumentReference" {
@@ -201,7 +201,7 @@ module "producer__readDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "read_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "producer__searchDocumentReference" {
@@ -230,7 +230,7 @@ module "producer__searchDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "search_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "producer__searchPostDocumentReference" {
@@ -259,7 +259,7 @@ module "producer__searchPostDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "search_post_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "producer__updateDocumentReference" {
@@ -289,7 +289,7 @@ module "producer__updateDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "update_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "producer__upsertDocumentReference" {
@@ -319,7 +319,7 @@ module "producer__upsertDocumentReference" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "upsert_document_reference.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 module "consumer__status" {
@@ -349,7 +349,7 @@ module "consumer__status" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "status.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
 
 
@@ -380,5 +380,5 @@ module "producer__status" {
     module.firehose__processor.firehose_subscription
   ]
   handler   = "status.handler"
-  retention = local.is_ephemeral ? 90 : 15
+  retention = local.is_persistent ? 90 : 7
 }
