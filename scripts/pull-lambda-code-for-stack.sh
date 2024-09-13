a80f2b3d5bc431e7fe60e256da75dcc575cf265f
Matt Dean <matt.dean3@nhs.net>
2024-09-13 14:03:22 +0100

    [NRL-793] WIP workflow for building permissions

---
+++ scripts/pull-lambda-code-for-stack.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Pull down all the lambda code for the named stack
+set -o errexit -o nounset -o pipefail
+
+: ${DIST_DIR:="./dist"}
+
+stack_name="$1"
+
+function pull_lambda_code(){
+    local api_name="$1"
+    local endpoint_name="$2"
+    local lambda_name="nhds-nrlf--${stack_name}--API-${api_name}--${endpoint_name}"
+
+    echo "Downloading code for lambda ${lambda_name}...."
+    code_url="$(aws lambda get-function  --function-name ${lambda_name} | jq -r .Code.Location)"
+    curl "${code_url}" > "${DIST_DIR}/${api_name}-${endpoint_name}.zip"
+}
+
+function pull_layer_code(){
+    local name="$1"
+    local layer_name="nhds-nrlf--${stack_name}--${name}"
+    local layer_version="$(aws lambda list-layer-versions --layer-name ${layer_name} | jq -r '.LayerVersions[0].Version')"
+    local layer_pkg_name="$(echo ${layer_name} | tr '-' '_').zip"
+
+    echo "Downloading code for layer ${layer_name} version ${layer_version}...."
+    code_url="$(aws lambda get-layer-version --layer-name ${layer_name} --version-number ${layer_version} | jq -r .Content.Location)"
+    curl "${code_url}" > "${DIST_DIR}/${layer_pkg_name}"
+}
+
+mkdir -p "${DIST_DIR}"
+
+echo "Pulling code for consumer API lambdas...."
+for endpoint_name in $(ls api/consumer)
+do
+    pull_lambda_code "consumer" "${endpoint_name}"
+done
+
+echo "Pulling code for producer API lambdas...."
+for endpoint_name in $(ls api/producer)
+do
+    pull_lambda_code "producer" "${endpoint_name}"
+done
+
+echo "Pulling code for layers...."
+for layer_name in nrlf dependency-layer nrlf-permissions
+do
+    pull_layer_code "${layer_name}"
+done
