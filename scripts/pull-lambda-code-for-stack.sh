f27eeaa80d1ff93a1a4df400ba30f3c67e43ad3f
Matt Dean <matt.dean3@nhs.net>
2024-09-17 12:34:19 +0100

    [NRL-793] Complete update-lambda-permissions workflow and pull-lambda-code-for-stack.sh script

--- scripts/pull-lambda-code-for-stack.sh
+++ scripts/pull-lambda-code-for-stack.sh
@@ -9,40 +9,57 @@ stack_name="$1"
 function pull_lambda_code(){
     local api_name="$1"
     local endpoint_name="$2"
-    local lambda_name="nhds-nrlf--${stack_name}--API-${api_name}--${endpoint_name}"
+    local lambda_name="nhsd-nrlf--${stack_name}--api--${api_name}--${endpoint_name}"
 
-    echo "Downloading code for lambda ${lambda_name}...."
+    echo -n "- Downloading code for lambda ${lambda_name}.... "
     code_url="$(aws lambda get-function  --function-name ${lambda_name} | jq -r .Code.Location)"
-    curl "${code_url}" > "${DIST_DIR}/${api_name}-${endpoint_name}.zip"
+    curl "${code_url}" 2>/dev/null > "${DIST_DIR}/${api_name}-${endpoint_name}.zip"
+    echo "✅"
 }
 
 function pull_layer_code(){
     local name="$1"
-    local layer_name="nhds-nrlf--${stack_name}--${name}"
+    local layer_name="nhsd-nrlf--${stack_name}--${name}"
     local layer_version="$(aws lambda list-layer-versions --layer-name ${layer_name} | jq -r '.LayerVersions[0].Version')"
-    local layer_pkg_name="$(echo ${layer_name} | tr '-' '_').zip"
+    local layer_pkg_name="$(echo ${name} | tr '-' '_').zip"
 
-    echo "Downloading code for layer ${layer_name} version ${layer_version}...."
+    echo -n "- Downloading code for layer ${layer_name} version ${layer_version}...."
     code_url="$(aws lambda get-layer-version --layer-name ${layer_name} --version-number ${layer_version} | jq -r .Content.Location)"
-    curl "${code_url}" > "${DIST_DIR}/${layer_pkg_name}"
+    curl "${code_url}" 2>/dev/null > "${DIST_DIR}/${layer_pkg_name}"
+    echo "✅"
 }
 
 mkdir -p "${DIST_DIR}"
 
+echo
 echo "Pulling code for consumer API lambdas...."
 for endpoint_name in $(ls api/consumer)
 do
+    if [ ! -d "api/consumer/${endpoint_name}" ]; then
+        continue
+    fi
+
     pull_lambda_code "consumer" "${endpoint_name}"
 done
 
+echo
 echo "Pulling code for producer API lambdas...."
 for endpoint_name in $(ls api/producer)
 do
+    if [ ! -d "api/producer/${endpoint_name}" ]; then
+        continue
+    fi
+
     pull_lambda_code "producer" "${endpoint_name}"
 done
 
+echo
 echo "Pulling code for layers...."
 for layer_name in nrlf dependency-layer nrlf-permissions
 do
     pull_layer_code "${layer_name}"
 done
+
+echo
+echo "✅ Done. Code is in ${DIST_DIR}"
+echo
